691ccf3e37ac342ebfdd95655a762e1350261343
GluttonyD <aritomo.san@gmail.com>
2019-04-18 11:46:18 +0300

    docker fix

--- src/testing/backend/views/layouts/left.php
+++ src/testing/backend/views/layouts/left.php
@@ -57,34 +57,6 @@ $user = Yii::$app->user->getIdentity();
                     ],
                     ['label' => 'Разделы', 'icon' => 'folder-o', 'url' => ['/section/index']],
                     ['label' => 'Вопросы', 'icon' => 'question', 'url' => ['/question/index']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
                 ],
 
             ]
